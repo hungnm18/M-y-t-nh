b6c049db3ea9bd5c477e8df8dc553ea32bb61483
Mhung <hungsh18@gmail.com>
2026-08-08 17:16:29 +0700

    Delete test_db.php

--- test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=127.0.0.1;dbname=shop_giay;charset=utf8", "root", "");
-    $stmt = $pdo->query('SELECT * FROM categories');
-    print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
